83389759513251e1684305e37c16889e1d2f0552
RKoekemoer <rkoekemoer1994@gmail.com>
2025-07-23 21:35:30 +0200

    Desktop: full animations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+/* Slide in animations */
+$(document).ready(function () {
+
+    const education = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(( entry,index )=> {
+            if (entry.isIntersecting) {
+
+                const delay = index * 750;
+
+                setTimeout(() => {
+                    $(entry.target)
+                        .addClass('animate__slideInRight')
+                        .css('visibility', 'visible');
+                }, delay);
+
+            }
+        });
+    }, {
+        threshold: 0
+    });
+
+    $('#education').find('.educationItem').each(function () {
+        education.observe(this);
+    });
+
+});
+
+/* Skills matrix - numbering */
 $(document).ready(function () {
 
     const skillsMatrix = new IntersectionObserver(function (entries, observer) {
@@ -7,7 +35,6 @@ $(document).ready(function () {
                 var item = $(entry.target).find('.skillMastery');
 
                 item.html( item.data('fill') );
-
                 $(entry.target).find('.filler').css('width', item.data('fill'));
 
             }
